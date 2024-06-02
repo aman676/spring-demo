5d65926787edb2e90db5a2dbf2758eb67a440f13
aman676 <amanmahajan676@gmail.com>
2024-06-02 16:59:41 +0530

    first commit

---
+++ src/main/java/com/warshaw/learn_spring/App02HelloSpring.java
@@ -0,0 +1,10 @@
+package com.warshaw.learn_spring;
+
+public class App02HelloSpring {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/main/java/com/warshaw/learn_spring/game/GameConsole.java
@@ -0,0 +1,8 @@
+package com.warshaw.learn_spring.game;
+
+public interface GameConsole {
+	void up();
+	void down();
+	void left();
+	void right();
+}
